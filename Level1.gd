8ce735c364b3e723b4623b3e01b2c7122fd285a0
asm0dey <pavel.finkelshtein@gmail.com>
2020-02-01 14:12:50 +0300

    Adds connection of all organs to all beds

---
+++ Level1.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for organ in get_tree().get_nodes_in_group("organs"):
+		for bed in get_tree().get_nodes_in_group("beds"):
+			organ.connect("organ_released",bed,"_organ_released")
+			organ.connect("organ_taken",bed,"_organ_taken")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
