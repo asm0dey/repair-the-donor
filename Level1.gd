6f973903b348073e754695d40c974cbffd473e84
asm0dey <pavel.finkelshtein@gmail.com>
2020-02-01 14:29:48 +0300

    Adds arrays with organs to containers

--- Level1.gd
+++ Level1.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	for organ in get_tree().get_nodes_in_group("organs"):
 		for bed in get_tree().get_nodes_in_group("beds"):
 			organ.connect("organ_released",bed,"_organ_released")
--- OrganContainer.gd
+++ OrganContainer.gd
@@ -2,13 +2,17 @@ extends StaticBody2D
 
 export (int) var size
 var _mouse_in = false
-export (String) var container_name = "bed"
+export (String) var container_name
+export (float) var decrement_per_iteration
+var organs = []
 
 func _organ_taken(organ, _event):
 	if _mouse_in:
-		print("organ " + organ.organ_name + " is get of " + container_name)
+		organs.remove(organs.find(organ))
+		print("organ " + organ.organ_name + " is got of " + container_name)
 
 
 func _organ_released(organ, _event):
 	if _mouse_in:
+		organs.append(organ)
 		print("organ " + organ.organ_name + " is put on " + container_name)
