7d3f7481970461ffc745d582c37d4c8967624643
asm0dey <pavel.finkelshtein@gmail.com>
2020-02-02 15:36:51 +0300

    Adds second level
    Signed-off-by: asm0dey <pavel.finkelshtein@gmail.com>

--- Level2.gd
+++ Level2.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-var starting_time = 100
+var starting_time = 200
 var time_step = 10
 var current_time_left
 
-var bed_groups = ["b1","b2"]
+var bed_groups = ["b1","b2",'b3']
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,48 +12,38 @@ func _ready():
 	adjust_timer()
 	assign_organ_actions()
 	assign_organs()
-	
-	_on_OrganCountUpdateTimer_timeout()
+
 
 func adjust_timer():
 	if current_time_left < 0:
 		current_time_left = 0
-	$GUI/HBoxContainer/Bars/Bar/Count/Background/Number.text = "%s" % current_time_left
-	$GUI/HBoxContainer/Bars/Bar/Gauge.value = current_time_left
+
 
 func assign_organ_actions():
 	for organ in get_tree().get_nodes_in_group("organs"):
 		for bed in get_tree().get_nodes_in_group("beds"):
+			print("connecting "+organ.organ_name+" ro bed "+bed.container_name)
 			organ.connect("organ_released", bed, "_organ_released")
 			organ.connect("organ_taken", bed, "_organ_taken")
 
 func assign_organs():
 	for group in bed_groups:
 		var organs = []
+		var fake_organs = []
 		var bed
 		for obj in get_tree().get_nodes_in_group(group):
 			if obj is StaticBody2D:
 				bed = obj
 			elif obj is KinematicBody2D:
-				organs.append(obj)
+				if obj.to_delete:
+					fake_organs.append(obj)
+				else:
+					organs.append(obj)
 				obj.z_index += 1
 		bed.organs = organs
 		for organ in organs:
 			bed.places[organ.type] = organ.position
 			organ.container = bed
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	if randf() > 0.7:
-
-func _on_HealthUpdateTimer_timeout():
-	current_time_left = current_time_left - time_step
-	adjust_timer()
-
-func _on_Button_pressed():
-	get_tree().change_scene("res://Level2.tscn")
-
-
-func _on_OrganCountUpdateTimer_timeout():
-	var num_alive_organs = globals.count_alive_organs(bed_groups)
-	$GUI/HBoxContainer/Counters/Counter/Background/Number.text = "%s" % num_alive_organs
+		for organ in fake_organs:
+			bed.places[organ.type] = organ.position
+			organ.queue_free()
