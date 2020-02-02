11fdd0e7fcd27453eec66ac01b52a33900bc9e2a
asm0dey <pavel.finkelshtein@gmail.com>
2020-02-02 15:54:16 +0300

    Adds GUI to level 2

--- Level2.gd
+++ Level2.gd
@@ -17,12 +17,13 @@ func _ready():
 func adjust_timer():
 	if current_time_left < 0:
 		current_time_left = 0
-
+	$GUI/HBoxContainer/Bars/Bar/Count/Background/Number.text = "%s" % current_time_left
+	
+	$GUI/HBoxContainer/Bars/Bar/Gauge.value = float(current_time_left) / starting_time * 100
 
 func assign_organ_actions():
 	for organ in get_tree().get_nodes_in_group("organs"):
 		for bed in get_tree().get_nodes_in_group("beds"):
-			print("connecting "+organ.organ_name+" ro bed "+bed.container_name)
 			organ.connect("organ_released", bed, "_organ_released")
 			organ.connect("organ_taken", bed, "_organ_taken")
 
@@ -47,3 +48,17 @@ func assign_organs():
 		for organ in fake_organs:
 			bed.places[organ.type] = organ.position
 			organ.queue_free()
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Level3.tscn")
+
+
+func _on_HealthUpdateTimer_timeout():
+	current_time_left = current_time_left - time_step
+	adjust_timer()
+
+
+func _on_OrganCountUpdateTimer_timeout():
+	var num_alive_organs = globals.count_alive_organs(bed_groups)
+	$GUI/HBoxContainer/Counters/Counter/Background/Number.text = "%s" % num_alive_organs
