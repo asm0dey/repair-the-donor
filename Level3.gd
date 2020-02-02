bb380e186165bb02e402cbb10aafefcece5347c3
asm0dey <pavel.finkelshtein@gmail.com>
2020-02-02 10:52:37 +0300

    Adds organ binding to level 3
    Signed-off-by: asm0dey <pavel.finkelshtein@gmail.com>

---
+++ Level3.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	assign_organ_actions()
+	assign_organs()
+	
+func assign_organ_actions():
+	for organ in get_tree().get_nodes_in_group("organs"):
+		for bed in get_tree().get_nodes_in_group("beds"):
+			print("connecting "+organ.organ_name+" ro bed "+bed.container_name)
+			organ.connect("organ_released", bed, "_organ_released")
+			organ.connect("organ_taken", bed, "_organ_taken")
+
+func assign_organs():
+	for group in ["b1","b2",'b3','b4']:
+		var organs = []
+		var bed
+		for obj in get_tree().get_nodes_in_group(group):
+			if obj is StaticBody2D:
+				bed = obj
+			elif obj is KinematicBody2D:
+				organs.append(obj)
+				obj.z_index += 1
+		bed.organs = organs
+		for organ in organs:
+			bed.places[organ.type] = organ.position
